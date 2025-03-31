824e508671a22d0e46f38647f67d58e71a9744d6
MarcusC001 <marcuscolmenero884@gmail.com>
2025-03-31 16:56:49 +0000

    Mon, Mar 31, 2025, 9:56 AM -07:00

--- website1/javascript/relaxer.js
+++ website1/javascript/relaxer.js
@@ -4,4 +4,8 @@ const text = document.getElementById('text')
 function breathAnimation() {
     text.innerHTML = 'Breathe In'
     container.classList.add('grow')
+
+    setTimeout( () => {
+        text.innerHTML = 'Hold'
+    }, 3000)
 }
